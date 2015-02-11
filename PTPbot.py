9ac43df9276f0c984fe689e76beb40582394c90b
Tyler Standridge <tstand90@gmail.com>
2015-02-10 22:23:57 -0800

    Added some methods and pep8 cleanup

--- PTPbot.py
+++ PTPbot.py
@@ -17,24 +17,46 @@ class PTPbot(object):
         self.subreddit = self.reddit.get_subreddit(self.config.get('subreddit', 'subred'))
         self.postTitle = self.config.get('postTitle', 'title')
 
-        login(self.reddit, self.config)      
+        login(self.reddit, self.config)
         logging.info("Login Success")
+
     # get the desired post
     def get_post(self, post_title):
-       return self.subreddit.search(post_title)
+        return self.subreddit.search(post_title)
 
-    
-    def post_flair(user):
+    def post_flair(self, user):
+        pass
 
-    def submit_Post():
+    def submit_Post(self):
+        pass
 
+    def get_submission(self, submission_id):
+        submission = self.reddit.get_submission(submission_id=submission_id)
+
+        return submission
+
+    def compile_winning_story(self, submission):
+        '''
+        Compile the winning story from the comments in the post into one story
+        '''
+
+        winning_comments = submission.comments
+        full_story = ''
+
+        # NOTE: Need to iterate down the comment tree, right now it's just
+        # printing the first comment
+
+        for comment in winning_comments:
+            full_story += comment.body + '\n'
+
+        return full_story
 
 
 def main():
     # establish logging
     logging.basicConfig(filename="PTP.log", level=logging.INFO)
-    logging.info("Starting New Logging Session")    
-    print "Hello World"
+    logging.info("Starting New Logging Session")
+    print("Hello World")
     reddit = praw.Reddit("r/passtheparagraph bot by u/broketheweb and u/AetherGrey")
     # initiate bot here, pass reddit obj and config file location
     config = ConfigParser.ConfigParser()
@@ -42,21 +64,21 @@ def main():
     config.read("conf.cnf")
     bot = PTPbot(reddit, config)
     # the inital(weekly) post will always have the same title
-    post = bot.get_Post(config.get('postTitle', 'Title');
+    post = bot.get_Post(config.get('postTitle', 'Title'))
     logging.info("Post success")
 
+
 def login(reddit, conf):
-    print "Login"
+    print("Login")
     logging.info(conf.get('account', 'uname'))
-    return reddit.login(conf.get('account','uname'), conf.get('account','pwd'))
+    return reddit.login(conf.get('account', 'uname'), conf.get('account', 'pwd'))
 
 
 def count_votes(post):
-    # count the votes in the post
-	
-
-#compile the winning story from the comments in the post
-def compile_story(post):
+    '''
+    Count the votes in the post
+    '''
+    pass
 
 
 if __name__ == "__main__":
